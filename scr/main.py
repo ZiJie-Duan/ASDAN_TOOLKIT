9319fce1c9d41545262ff127ccc9b1207567e4d6
lucycore <32613263+lucycore@users.noreply.github.com>
2020-11-27 21:12:28 +0800

    数据准备

--- scr/main.py
+++ scr/main.py
@@ -1,4 +1,2 @@
+#ASDAN 商赛辅助分析程序
 
-for time in range(200):
-	print("\n这是第" + str(time) + "次问Helen数学作业")
-	print("Helen 你好！数学作业是什么？")
---
+++ test/函数列表测试.py
@@ -0,0 +1,9 @@
+
+
+def function(data):
+    print("print by function: {}".format(data))
+
+test_list = ["hello",function]
+
+test_list[1](test_list[0])
+
